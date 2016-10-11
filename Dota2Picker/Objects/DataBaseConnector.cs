42317c5d070db944e2e66f11c02dab1af2fcec04
Ramzi Meyzinski <Ramzi Meyzinski>
2016-10-11 19:04:34 +0300

    Database connector refactor.

--- Dota2Picker/Objects/DataBaseConnector.cs
+++ Dota2Picker/Objects/DataBaseConnector.cs
@@ -39,6 +39,57 @@ namespace Dota2Picker.Objects
             return heroes;
         }
 
+        public List<Hero> getHeroesByStrength()
+        {
+            List<Hero> heroes = new List<Hero>();
+
+            //< get records > 
+            string query = @"SELECT hero.id, hero.name, hero.attack_id, hero.attribute_id, hero.role_id, attack.type AS type_name, attribute.name AS attribute_name FROM 
+                            Heroes hero, AttackType attack, Attribute attribute Where hero.attack_id = attack.id AND
+                            hero.attribute_id = attribute.id AND hero.attribute_id = 1";
+
+            using (var statement = new SQLite.SQLiteConnection(dbName))
+            {
+                heroes = statement.Query<Hero>(query);
+            }
+
+            return heroes;
+        }
+
+        public List<Hero> getHeroesByAgility()
+        {
+            List<Hero> heroes = new List<Hero>();
+
+            //< get records > 
+            string query = @"SELECT hero.id, hero.name, hero.attack_id, hero.attribute_id, hero.role_id, attack.type AS type_name, attribute.name AS attribute_name FROM 
+                            Heroes hero, AttackType attack, Attribute attribute Where hero.attack_id = attack.id AND
+                            hero.attribute_id = attribute.id AND hero.attribute_id = 2";
+
+            using (var statement = new SQLite.SQLiteConnection(dbName))
+            {
+                heroes = statement.Query<Hero>(query);
+            }
+
+            return heroes;
+        }
+
+        public List<Hero> getHeroesByIntelligence()
+        {
+            List<Hero> heroes = new List<Hero>();
+
+            //< get records > 
+            string query = @"SELECT hero.id, hero.name, hero.attack_id, hero.attribute_id, hero.role_id, attack.type AS type_name, attribute.name AS attribute_name FROM 
+                            Heroes hero, AttackType attack, Attribute attribute Where hero.attack_id = attack.id AND
+                            hero.attribute_id = attribute.id AND hero.attribute_id = 3";
+
+            using (var statement = new SQLite.SQLiteConnection(dbName))
+            {
+                heroes = statement.Query<Hero>(query);
+            }
+
+            return heroes;
+        }
+
         //Get all heroes that picked hero is weak against
         public List<Hero> GetWeakAgainst(int heroIndex)
         {
